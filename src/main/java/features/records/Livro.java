1e7935736228e2cdb83cf634f015f552694ef754
Anjos <deusimar.anjos@capgemini.com>
2020-04-25 15:16:48 -0300

    Implmentacao sobrescrita no metodo toString

--- src/main/java/features/records/Livro.java
+++ src/main/java/features/records/Livro.java
@@ -2,5 +2,23 @@ package features.records;
 
 import java.util.Set;
 
-public record Livro(String titulo, String isbn, String genero, Editora editora, Set<Autor>autores) {
+public record Livro(Long id, String titulo, String isbn, String genero, Editora editora, Set<Autor>autores) {
+    @Override
+    public String toString() {
+        return String.format("""
+                        Livro{
+                            id: %s,
+                            titulo: %s,
+                            isbn: %s,
+                            genero: %s,
+                            editora: %s,
+                            autores: %s,
+                        }""",
+                id,
+                titulo,
+                isbn,
+                genero,
+                editora,
+                autores);
+    }
 }
